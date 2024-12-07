24eb2fcd4736b728406e5a6020286027b0aa9901
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2024-12-07 18:37:37 +0300

    initial commit

---
+++ src/main/java/com/kadiraksoy/todoapp/TodoAppMultiTenancyApplication.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppMultiTenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppMultiTenancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadiraksoy/todoapp/TodoAppMultiTenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppMultiTenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
